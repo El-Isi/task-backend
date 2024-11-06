5b8d6b8b8e24b9f7e2acaf4963a41b7303c670b1
Isidro Fernando Gonzalez Gomez <isidro.f.gonzalez@gmail.com>
2024-11-06 13:56:54 -0600

    fix: Change sentences to english

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ import app from './app';
 const PORT = 3001;
 
 app.use((req, res, next) => {
-    console.log(`Ruta no encontrada: ${req.originalUrl}`);
+    console.log(`Route not found: ${req.originalUrl}`);
     res.status(404).json({ message: 'Ruta no encontrada' });
 });
   
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
